54fe29d0d3670a0335c372753908d76f8d22d5a0
ababd <gongodaoluren3@gmail.com>
2024-02-20 00:06:26 +0900

    サインアップ、ログインページを作りました。

--- README.md
+++ README.md
@@ -1,40 +1,38 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# full-stack-dev practice
 
-## Getting Started
+バックエンド用レポジトリです。
 
-First, run the development server:
+## Techs
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+Node.js, MySQL
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Develop
 
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
+### Git branch
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
+#### Main Branch
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+開発時は、`main`ブランチからブランチを切ってください。
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+`main`: プロダクション用ブランチ
 
-## Learn More
+`develop`: 次のリリースに備え、最新のソースコードを管理するブランチ
 
-To learn more about Next.js, take a look at the following resources:
+#### Support Branch
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+`feature`: 新しい機能の開発や簡単なバグ修正などの開発作業を行うためのブランチ。
+develop ブランチからの分岐として作成され、develop ブランチにマージする。
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+`release`: リリース作業を行うためのブランチ。
+develop ブランチからの分岐として作成され、develop ブランチと master ブランチにマージする。
 
-## Deploy on Vercel
+`hot-fix`: 緊急の修正作業を行うためのブランチ。
+master ブランチからの分岐として作成され、develop ブランチと master ブランチにマージする。
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+分け方は GitHub Flow を参照 URL : https://www.kagoya.jp/howto/it-glossary/develop/githubflow/
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+## Run
+
+`npm install`\
+`npm run start` : テスト\
+`http://localhost:3000`
---
+++ src/pages/signup.tsx
@@ -0,0 +1,79 @@
+import React, {ReactHTMLElement, useState} from "react";
+import { useRouter } from 'next/router';
+
+const Registar = () => {
+    const [username, setUsername] = useState<string>("");
+    const [email, setEmail] = useState<string>("")
+    const [password, setPassword] = useState<string>("");
+    const [error, setError] = useState<string>("");
+
+    // ROUTER SETTING
+    const router = useRouter();
+
+    // フォームデータをAPI側にリクエストを送る
+    const submitHandler = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
+        e.preventDefault();
+        try {
+            const res = await fetch("http://localhost:8081/auth/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username,
+                    email,
+                    password,
+                })
+            });
+    
+            // レスポンスの処理を追加する
+            const data = await res.json();
+            if (data.token) {
+                router.push("/login");
+            } else {
+                setError(data.message);
+            }
+
+        } catch (error) {
+            console.error("エラーが発生しました:", error);
+        }
+    };
+
+    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const {name, value} = e.target;
+
+        switch (name) {
+            case "username":
+                setUsername(value);
+                break;
+            case "email":
+                setEmail(value);
+                break;
+            case "password":
+                setPassword(value);
+                break;
+        }
+    }
+    
+    return (
+        <>
+            <h1>SIGN UP</h1>
+            <form onSubmit={submitHandler}>
+                <label htmlFor="username">ユーザー名</label>
+                <input onChange={changeHandler} value={username} type="text" name="username" id="username" />
+                <label htmlFor="email">メールアドレス</label>
+                <input onChange={changeHandler} value={email} type="email" name="email" id="email" />
+                <label htmlFor="password">パスワード</label>
+                <input onChange={changeHandler} value={password} type="password" name="password" id="password" />
+                {error && <div>{error}</div>}
+                <button type="submit">
+                    SUBMIT
+                </button>
+            </form>
+
+            <a href="http://localhost:3000/login">アカウントを持っている:LOGIN</a>
+        </>
+    );
+}
+
+export default Registar
